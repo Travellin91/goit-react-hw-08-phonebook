b63ec946ea05b6ed1412bf50f34b5f5dc25b448d
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-07-19 13:24:58 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,60 @@
-import React, { useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux';
-import store from '../redux/store';
-import { fetchContacts } from '../redux/contactsSlice';
-import ContactForms from './ContactForms/ContactForms';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
 import './app.css';
 
-function App() {
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { refreshUser } from '../redux/auth/operations';
+import SharedLayout from '../components/SharedLayout';
+import PrivateRoute from './Menu/PrivateRoute';
+import PublicRoute from './Menu/PublicRoute';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const Phonebook = lazy(() => import('../pages/Phonebook'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const RegistrationPage = lazy(() => import('../pages/Registration'));
+
+const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+    dispatch(refreshUser());
+  }, [dispatch])
 
   return (
-    <Provider store={store}>
-      <div className="app-container">
-        <h1 className="app-heading">PhoneBook</h1>
-        <ContactForms />
-        <Filter />
-        <ContactList />
-      </div>
-    </Provider>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/phonebook"
+          element={
+            <PrivateRoute redirectTo="/login" component={<Phonebook />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute
+              redirectTo="/phonebook"
+              restricted
+              component={<LoginPage />}
+            />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute
+              redirectTo="/phonebook"
+              restricted
+              component={<RegistrationPage />}
+            />
+          }
+        />
+        <Route path="*" element={<HomePage />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactsReducer from './contactsSlice';
-import filterReducer from './filterSlice';
+import { configureStore} from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactsSlice, filterSlice } from './contacts/slice';
+import { authSlice } from './auth/slice';
 
-const store = configureStore({
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
+    auth: persistReducer(authPersistConfig, authSlice.reducer),
+    contacts: contactsSlice.reducer,
+    filter: filterSlice.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
