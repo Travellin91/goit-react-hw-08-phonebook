460ad56e2aeeab7488d97cf0601324c3010776fa
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-07-19 15:26:38 +0300

    +

--- src/components/Button/Styles.js
+++ src/components/Button/Styles.js
@@ -1,4 +1,16 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const hoverAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.05);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
 
 export const StyledButton = styled.button`
   border: none;
@@ -9,7 +21,11 @@ export const StyledButton = styled.button`
   font-size: 16px;
   font-weight: 700;
   color: whitesmoke;
+  cursor: pointer;
+  animation: ${hoverAnimation} 1s infinite;
+
   :hover {
     box-shadow: inset 0px 0px 10px 5px rgba(0, 0, 0, 0.1);
+    animation: none;
   }
 `;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,78 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { Notify } from 'notiflix';
+import { addContact } from '../../redux/contacts/operations';
+import Button from '../Button';
+import { StyledForm } from './Styles';
+
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const contacts = useSelector(state => state.contacts.items);
+  const dispatch = useDispatch();
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      Notify.failure(`${name} Вже додано до списку контактів`);
+      return;
+    }
+    dispatch(addContact({ name: name, number: number }));
+    setName('');
+    setNumber('');
+  };
+
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return console.log('Відбулася непередбачена ситуація');
+    }
+  };
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <label className="formLabel">
+        <span className="labelTitle">Ім'я:</span>
+        <input
+          className="inputTag"
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Ім'я може містити лише літери, апостроф, тире та пробіли. Наприклад, Адріан, Джейкоб Мерсер, Шарль де Бац де Кастельмор д'Артаньян."
+          onChange={handleChange}
+          value={name}
+          required
+        />
+      </label>
+      <label className="formLabel">
+        <span className="labelTitle">Номер:</span>
+        <input
+          className="inputTag"
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефону повинен містити лише цифри і може містити пробіли, тире, дужки та може починатися з +"
+          onChange={handleChange}
+          value={number}
+          required
+        />
+      </label>
+
+      <Button title="Добавити контакт" type="submit" />
+    </StyledForm>
+  );
+};
+
+export default ContactForm;
