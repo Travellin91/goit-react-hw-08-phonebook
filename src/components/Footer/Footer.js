c12e6ab58f8a2a6ded6dd4954038a76a13c12985
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-07-20 13:06:08 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,6 @@ const Footer = styled.footer`
   color: black;
   padding: 20px;
   text-align: center;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
   max-width: 480px;
   margin: 0 auto;
   border-top: 3px solid lightgray;
