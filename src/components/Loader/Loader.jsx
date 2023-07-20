ae173b9d14e78dd5e1cdd0acf45706881dd4b134
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-07-20 11:54:32 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,17 @@
-import { ThreeDots } from 'react-loader-spinner';
+import { Audio } from 'react-loader-spinner';
 import { StyledLoader } from './LoaderStyles';
 
 export default function Loader() {
   return (
     <StyledLoader>
-      <ThreeDots
-        height="80"
-        width="80"
-        radius="9"
-        color="greenyellow"
-        ariaLabel="three-dots-loading"
+      <Audio
+        height="100"
+        width="100"
+        color="#ff6b6b"
+        ariaLabel="audio-loading"
+        wrapperStyle={{}}
+        wrapperClass="wrapper-class"
+        visible={true}
       />
     </StyledLoader>
   );
